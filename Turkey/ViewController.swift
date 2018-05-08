a900f0e0f3c69c0340d689399c3474884fc8ccad
numa08 <n511287@gmail.com>
2018-05-08 17:08:14 +0900

    Initial Commit

---
+++ Turkey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turkey
+//
+//  Created by numa08 on 2018/05/08.
+//  Copyright © 2018年 numa08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
