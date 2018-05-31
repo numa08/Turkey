cc709a81537dd50922ad970cab9cbf343892672f
numa08 <n511287@gmail.com>
2018-05-31 14:16:58 +0900

    face detection

--- Turkey/ViewController.swift
+++ Turkey/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Tello
 import AVFoundation
 import AVKit
+import Vision
 
 class ViewController: UIViewController,
 TelloConnectedEventHandlerProtocol,
@@ -20,7 +21,17 @@ ImageDecoderDelegate
     @IBOutlet weak var image: UIImageView!
     var videoData = NSMutableData()
     let imageDecoder = ImageDecoder()
-    
+    lazy var highlightView: UIView = {
+        let v = UIView()
+        v.layer.borderColor = UIColor.blue.cgColor
+        v.layer.borderWidth = 4
+        v.layer.backgroundColor = UIColor.clear.cgColor
+        return v
+    }()
+    private var requestHandler: VNSequenceRequestHandler = VNSequenceRequestHandler()
+    private var lastObservation: VNDetectedObjectObservation?
+    private var isTouched: Bool = false
+    private let objectDetectionQueue = DispatchQueue(label: "object-detection")
     func conected() {
         print("Connected")
         TelloStartVideo(nil)
@@ -33,6 +44,20 @@ ImageDecoderDelegate
     func videoFrame(_ p0: Data!) {
         self.imageDecoder.onNewFrame(p0)
     }
+
+    private func drawFaceRectangle(image: UIImage?, observation: VNFaceObservation) -> UIImage? {
+        let imageSize = image!.size
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        image?.draw(in: CGRect(origin: .zero, size: imageSize))
+        context?.setLineWidth(4.0)
+        context?.setStrokeColor(UIColor.green.cgColor)
+        context?.stroke(observation.boundingBox.converted(to: imageSize))
+        let drawnImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return drawnImage
+    }
+    
     
     func decoder(_ decoder: ImageDecoder!, didDecode image: UIImage!) {
         DispatchQueue.main.async {
@@ -42,19 +67,11 @@ ImageDecoderDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.image.addSubview(self.highlightView)
         imageDecoder.delegate = self
         if !self.imageDecoder.open() {
             fatalError("failed open fifo")
         }
-
-//        let player = AVPlayer(url: URL(string: "udp://localhost:12345")!)
-//        let vc = AVPlayerViewController()
-//        vc.player = player
-//        vc.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-//        self.addChildViewController(vc)
-//        self.view.addSubview(vc.view)
-//        player.play()
-        
         DispatchQueue(label: "tello").async {
             TelloInitDrone("8090")
             TelloRegisterOnConnectedEvent(self, nil)
@@ -69,5 +86,15 @@ ImageDecoderDelegate
         // Dispose of any resources that can be recreated.
     }
 
+    private func convertedRect(rect: CGRect, to size: CGSize) -> CGRect {
+        // view 上の長方形に変換する
+        // 座標系変換のため、 Y 軸方向に反転する
+        return CGRect(x: rect.minX * size.width, y: (1.0 - rect.maxY) * size.height, width: rect.width * size.width, height: rect.height * size.height)
+    }
 }
 
+extension CGRect {
+    func converted(to size: CGSize) -> CGRect {
+        return CGRect(x: self.minX * size.width, y: (1 - self.maxY) + size.height, width: self.width * size.width, height: self.height * size.height)
+    }
+}
